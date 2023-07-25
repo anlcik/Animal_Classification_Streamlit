695d07b0dc60511e9aca50c22953736261f42cec
anlcik <anilcelik@hotmail.com>
2023-07-25 17:04:46 +0300

    Animal_Classification_Streamlit
    Animal_Classification project running with Streamlit

--- web/app.py
+++ web/app.py
@@ -5,14 +5,24 @@ from PIL import Image
 from tensorflow import keras
 from PIL import Image
 
+st.set_page_config(
+    page_title= "Animal Classification",
+    page_icon= ":cat:",
+    layout='centered'
+)
+
+#column yapımızı oluşturduk
+col1,col2 = st.columns(2)
+
 # modelimizi yükledik
 model = tf.keras.saving.load_model("./web/model/model.hdf5")
 
 # class isimlerimizi değişkene atadık
 class_names = ['at', 'fil', 'inek', 'kedi', 'kelebek', 'kopek', 'koyun', 'orumcek', 'sincap', 'tavuk']
 
-# kullanıcıdan bir resim aldık
-uploaded_img =  st.file_uploader("Lütfen resim yükleyiniz.", type=['png','jpg'])
+with st.sidebar:
+    # kullanıcıdan bir resim aldık
+    uploaded_img =  st.file_uploader("Lütfen resim yükleyiniz.", type=['png','jpg'])
 
 if uploaded_img is not None:
 
@@ -23,8 +33,9 @@ if uploaded_img is not None:
     # ardından resmimizin boytunu değiştirdik
     resized_img = resized_img.resize((224,224))
 
-    # resmimizi ekranda gösterdik
-    st.image(resized_img)
+    # boyutlandırılmış resmimizi gösterdik
+    with col1:
+        st.image(resized_img)
 
     # resmimizi numpy dizisine dönüştürdük
     resized_img = tf.keras.utils.img_to_array(resized_img)
@@ -32,8 +43,44 @@ if uploaded_img is not None:
     #
     resized_img = np.expand_dims(resized_img, axis=0)
 
+    # sonucumuzu aldık ve bir değişkene atadık
     result = model.predict(resized_img)
     result = class_names[np.argmax(result[0])]
 
+    st.divider()
+
     # sonucu ekrana yazdırdık
     st.text("Bence, bu bir {}.".format(result))
+
+    # sonuçtaki hayvana göre hayvan resmini gösterdik
+    with col2:
+        if result == "at":
+            result_img = Image.open("images/at.jpeg")
+            st.image(result_img.resize((224,224)))
+        elif result == "fil":
+            result_img = Image.open("images/fil.jpeg")
+            st.image(result_img.resize((224,224)))
+        elif result == "inek":
+            result_img = Image.open("images/inek.jpeg")
+            st.image(result_img.resize((224,224)))
+        elif result == "kedi":
+            result_img = Image.open("images/kedi.jpeg")
+            st.image(result_img.resize((224,224)))
+        elif result == "kelebek":
+            result_img = Image.open("images/kelebek.png")
+            st.image(result_img.resize((224,224)))
+        elif result == "kopek":
+            result_img = Image.open("images/kopek.jpeg")
+            st.image(result_img.resize((224,224)))
+        elif result == "koyun":
+            result_img = Image.open("images/koyun.jpg")
+            st.image(result_img.resize((224,224)))
+        elif result == "orumcek":
+            result_img = Image.open("images/orumcek.jpg")
+            st.image(result_img.resize((224,224)))
+        elif result == "sincap":
+            result_img = Image.open("images/sincap.jpeg")
+            st.image(result_img.resize((224,224)))
+        else:
+            result_img = Image.open("images/tavuk.jpeg")
+            st.image(result_img.resize((224,224)))
